66eef547603bead8ccfd22c0a562905aa0065c6a
Sergey Sova <i.am@lestad.net>
2016-01-21 15:44:09 +0300

    Refactor array and console tests

--- test/array.js
+++ test/array.js
@@ -13,302 +13,304 @@ function beFalse(result) {
 }
 
 describe('Array', function() {
-  describe('.prototype.first', function() {
-    it('exists', function(){
-      should([1]).have.property('first');
-      should([1].first).be.a.Number;
-      should(["a"].first).be.a.String;
-      should([function(){}].first).be.a.Function;
-      should([null].first).be.a.Null;
-      should([]).have.property('first');
-      should([].first).be.a.Undefined;
-    });
-
-    it('get correct value', function(){
-      should([5, 2].first).equal(5);
-      should(["F", 2].first).equal("F");
-      should([9.4, 2].first).equal(9.4);
-      should([false, 2].first).false();
-    });
-
-    it('set value', function(){
-      var array = [1, 2, 3];
-
-      array.first = 10;
-      should(array.first).be.equal(10);
-      should(array[0]).be.equal(10);
-
-      array.first = undefined;
-      should(array.first).be.equal(undefined);
-      should(array[0]).be.equal(undefined);
-    });
-
-    it('create value', function(){
-      var array = [];
-
-      array.first = 2;
-      should(array[0]).equal(2);
-      should(array.length).equal(1);
-    });
-  });
-
-  describe('.prototype.second', function() {
-    it('exists', function(){
-      should([1, 2]).have.property('second');
-      should([1, 2].second).be.a.Number;
-      should(["a", "b"].second).be.a.String;
-      should([function(){}, function(){}].second).be.a.Function;
-      should([null, null].second).be.a.Null;
-
-      should([]).have.property('second');
-      should([2]).have.property('second');
-      should([].second).be.a.Undefined;
-      should([2].second).be.a.Undefined;
-    });
-
-    it('get correct value', function(){
-      should([5, 2].second).equal(2);
-      should(["F", "R"].second).equal("R");
-      should([9.4, 12.5].second).equal(12.5);
-      should([false, true].second).be.true();
-    });
-
-    it('set value', function(){
-      var array = [1, 2, 3];
-
-      array.second = 10;
-      should(array.second).be.equal(10);
-      should(array[1]).be.equal(10);
-
-      array.second = undefined;
-      should(array[1]).be.equal(undefined)
-    });
-
-    it('create value', function(){
-      var array = [];
-
-      array.second = 5;
-      should(array[0]).equal(undefined);
-      should(array[1]).equal(5);
-      should(array.length).equal(2);
-    });
-  });
-
-  describe('.prototype.last', function() {
-    it('exists', function(){
-      should([1, 2]).have.property('last');
-      should([1, 2].last).be.a.Number;
-      should(["a", "b"].last).be.a.String;
-      should([function(){}, function(){}].last).be.a.Function;
-      should([null, null].last).be.a.Null;
-
-      should([]).have.property('last');
-      should([1].last).be.a.Number;
-    });
-
-    it('get correct value', function(){
-      should([5, 2].last).equal(2);
-      should(["F", "R"].last).equal("R");
-      should([9.4, 12.5].last).equal(12.5);
-      should([false, true].last).be.true();
-    });
-
-    it('set value', function(){
-      var array = [1, 2, 3];
-
-      array.last = 10;
-      should(array.last).be.equal(10);
-      should(array[2]).be.equal(10);
-
-      array.last = undefined;
-      should(array.last).be.equal(undefined);
-      should(array[2]).be.equal(undefined);
-    });
-
-    it('not create value', function(){
-      var array = [];
-      should(array.last).equal(undefined);
-
-      array.last = 123;
-      should(array.last).equal(undefined);
-      should(array[0]).equal(undefined);
-      should(array.length).equal(0);
-    });
-  });
-
-  describe('.prototype.clean()', function() {
-    it('exists', function(){
-      should([]).have.property('clean');
-      should(["a", "b"]).have.property('clean');
-      should([].clean).should.be.Function;
-    });
-
-    var test = [1, "b", 3.14, null, {}, function(){}];
-
-    it('clean nothing', function(){
-      should(test).have.property('clean');
-      should.doesNotThrow(function(){
-        test.clean();
+  describe('.prototype', function() {
+    describe('.first', function() {
+      it('exists', function(){
+        should([1]).have.property('first');
+        should([1].first).be.a.Number;
+        should(["a"].first).be.a.String;
+        should([function(){}].first).be.a.Function;
+        should([null].first).be.a.Null;
+        should([]).have.property('first');
+        should([].first).be.a.Undefined;
       });
-      should(test.length).be.equal(6);
-      should(test[0]).be.equal(1);
-      should(test[test.length-1]).be.Function;
-    });
 
-    it('clean string', function(){
-      should.doesNotThrow(function(){
-        test.clean("b");
+      it('get correct value', function(){
+        should([5, 2].first).equal(5);
+        should(["F", 2].first).equal("F");
+        should([9.4, 2].first).equal(9.4);
+        should([false, 2].first).false();
       });
-      should(test.length).be.equal(5);
-      should(test[1]).be.equal(3.14);
-      should(test[test.length-1]).be.Function;
-    });
 
-    it('clean null', function(){
-      should.doesNotThrow(function(){
-        test.clean(null);
+      it('set value', function(){
+        var array = [1, 2, 3];
+
+        array.first = 10;
+        should(array.first).be.equal(10);
+        should(array[0]).be.equal(10);
+
+        array.first = undefined;
+        should(array.first).be.equal(undefined);
+        should(array[0]).be.equal(undefined);
       });
-      should(test.length).be.equal(4);
-      should(test[2]).be.Object;
-      should(test[test.length-1]).be.Function;
-    });
 
-    it('clean integer', function(){
-      should.doesNotThrow(function(){
-        test.clean(1);
+      it('create value', function(){
+        var array = [];
+
+        array.first = 2;
+        should(array[0]).equal(2);
+        should(array.length).equal(1);
       });
-      should(test.length).be.equal(3);
-      should(test[0]).be.equal(3.14);
-      should(test[test.length-1]).be.Function;
     });
 
-    it('clean float', function(){
-      should.doesNotThrow(function(){
-        test.clean(3.14);
+    describe('.second', function() {
+      it('exists', function(){
+        should([1, 2]).have.property('second');
+        should([1, 2].second).be.a.Number;
+        should(["a", "b"].second).be.a.String;
+        should([function(){}, function(){}].second).be.a.Function;
+        should([null, null].second).be.a.Null;
+
+        should([]).have.property('second');
+        should([2]).have.property('second');
+        should([].second).be.a.Undefined;
+        should([2].second).be.a.Undefined;
       });
-      should(test.length).be.equal(2);
-      should(test[0]).be.Object;
-      should(test[test.length-1]).be.Function;
-    });
 
-    it('not remove objects', function(){
-      should.doesNotThrow(function(){
-        test.clean({}); // Objects not equal
+      it('get correct value', function(){
+        should([5, 2].second).equal(2);
+        should(["F", "R"].second).equal("R");
+        should([9.4, 12.5].second).equal(12.5);
+        should([false, true].second).be.true();
       });
-      should(test.length).be.equal(2);
-      should(test[2]).be.Object;
-      should(test[test.length-1]).be.Function;
+
+      it('set value', function(){
+        var array = [1, 2, 3];
+
+        array.second = 10;
+        should(array.second).be.equal(10);
+        should(array[1]).be.equal(10);
+
+        array.second = undefined;
+        should(array[1]).be.equal(undefined)
+      });
+
+      it('create value', function(){
+        var array = [];
+
+        array.second = 5;
+        should(array[0]).equal(undefined);
+        should(array[1]).equal(5);
+        should(array.length).equal(2);
+      });
     });
 
-    it('clean by callback', function(){
+    describe('.last', function() {
+      it('exists', function(){
+        should([1, 2]).have.property('last');
+        should([1, 2].last).be.a.Number;
+        should(["a", "b"].last).be.a.String;
+        should([function(){}, function(){}].last).be.a.Function;
+        should([null, null].last).be.a.Null;
+
+        should([]).have.property('last');
+        should([1].last).be.a.Number;
+      });
+
+      it('get correct value', function(){
+        should([5, 2].last).equal(2);
+        should(["F", "R"].last).equal("R");
+        should([9.4, 12.5].last).equal(12.5);
+        should([false, true].last).be.true();
+      });
+
+      it('set value', function(){
+        var array = [1, 2, 3];
+
+        array.last = 10;
+        should(array.last).be.equal(10);
+        should(array[2]).be.equal(10);
+
+        array.last = undefined;
+        should(array.last).be.equal(undefined);
+        should(array[2]).be.equal(undefined);
+      });
+
+      it('not create value', function(){
+        var array = [];
+        should(array.last).equal(undefined);
+
+        array.last = 123;
+        should(array.last).equal(undefined);
+        should(array[0]).equal(undefined);
+        should(array.length).equal(0);
+      });
+    });
+
+    describe('.clean()', function() {
+      it('exists', function(){
+        should([]).have.property('clean');
+        should(["a", "b"]).have.property('clean');
+        should([].clean).should.be.Function;
+      });
+
       var test = [1, "b", 3.14, null, {}, function(){}];
 
-      should.doesNotThrow(function(){
-        test.clean(function(e){
-          return e > 1;
+      it('clean nothing', function(){
+        should(test).have.property('clean');
+        should.doesNotThrow(function(){
+          test.clean();
         });
+        should(test.length).be.equal(6);
+        should(test[0]).be.equal(1);
+        should(test[test.length-1]).be.Function;
       });
-      should(test.length).be.equal(5);
-      should(test[2]).be.equal(null);
-      should(test[test.length-1]).be.Function;
 
-      should.doesNotThrow(function(){
-        test.clean(function(e){
-          return typeof e === 'function';
-        })
+      it('clean string', function(){
+        should.doesNotThrow(function(){
+          test.clean("b");
+        });
+        should(test.length).be.equal(5);
+        should(test[1]).be.equal(3.14);
+        should(test[test.length-1]).be.Function;
       });
-      should(test.length).be.equal(4);
-      should(test[3]).be.Object;
-      should(test[test.length-1]).be.Object;
-    });
-  });
 
-  describe('.prototype.includes()', function() {
+      it('clean null', function(){
+        should.doesNotThrow(function(){
+          test.clean(null);
+        });
+        should(test.length).be.equal(4);
+        should(test[2]).be.Object;
+        should(test[test.length-1]).be.Function;
+      });
 
-    it('property exists', function(){
-        should([]).have.property('includes');
-        should(["a", "b"]).have.property('includes');
-        should([].includes).should.be.Function;
-    });
-    it('check return value', function(){
-      var result = ["a", "b"].includes("a");
-      should(result).be.a.Boolean;
-      beTrue(result);
-    });
-    it('single value check', function(){
-      result = ['a', 'b'].includes('b');
-      beTrue(result);
+      it('clean integer', function(){
+        should.doesNotThrow(function(){
+          test.clean(1);
+        });
+        should(test.length).be.equal(3);
+        should(test[0]).be.equal(3.14);
+        should(test[test.length-1]).be.Function;
+      });
 
-      result = ['a', 'b'].includes(['a']);
-      beTrue(result);
-    });
-    it('multi value check', function(){
-      result = ['a', 'b', 'c'].includes(['a', 'b', 'c']);
-      beTrue(result);
-    });
-    it('false check', function(){
-      result = ['a'].includes('b');
-      beFalse(result);
+      it('clean float', function(){
+        should.doesNotThrow(function(){
+          test.clean(3.14);
+        });
+        should(test.length).be.equal(2);
+        should(test[0]).be.Object;
+        should(test[test.length-1]).be.Function;
+      });
 
-      result = [].includes('c');
-      beFalse(result);
+      it('not remove objects', function(){
+        should.doesNotThrow(function(){
+          test.clean({}); // Objects not equal
+        });
+        should(test.length).be.equal(2);
+        should(test[2]).be.Object;
+        should(test[test.length-1]).be.Function;
+      });
 
-      result = [].includes(['a', 1]);
-      beFalse(result);
-    });
-  });
+      it('clean by callback', function(){
+        var test = [1, "b", 3.14, null, {}, function(){}];
 
+        should.doesNotThrow(function(){
+          test.clean(function(e){
+            return e > 1;
+          });
+        });
+        should(test.length).be.equal(5);
+        should(test[2]).be.equal(null);
+        should(test[test.length-1]).be.Function;
 
-  describe('.prototype.clone()', function(){
-    it('property exists', function(){
-      should([]).have.property('clone');
-      should([].clone).should.be.Function;
-    });
-
-    it('single item', function(){
-      var origin = [1];
-      var copy = origin.clone();
-
-      should(copy[0]).equal(1);
-      should(copy.length).equal(origin.length);
-      should(copy[0]).equal(origin[0]);
-    });
-
-    it('many items', function(){
-      var origin = [1, 2, 3];
-      var copy = origin.clone();
-
-      should(copy[0]).equal(1);
-      should(copy[1]).equal(2);
-      should(copy[2]).equal(3);
-      should(copy.length).equal(origin.length);
-      should(copy[0]).equal(origin[0]);
-      should(copy[1]).equal(origin[1]);
-      should(copy[2]).equal(origin[2]);
+        should.doesNotThrow(function(){
+          test.clean(function(e){
+            return typeof e === 'function';
+          })
+        });
+        should(test.length).be.equal(4);
+        should(test[3]).be.Object;
+        should(test[test.length-1]).be.Object;
+      });
     });
 
-    it('many objects', function(){
-      var origin = [{}, {}, {}];
-      var copy = origin.clone();
+    describe('.includes()', function() {
 
-      should(copy.length).equal(origin.length);
-      should(copy[0]).equal(origin[0]);
-      should(copy[1]).equal(origin[1]);
-      should(copy[2]).equal(origin[2]);
+      it('property exists', function(){
+          should([]).have.property('includes');
+          should(["a", "b"]).have.property('includes');
+          should([].includes).should.be.Function;
+      });
+      it('check return value', function(){
+        var result = ["a", "b"].includes("a");
+        should(result).be.a.Boolean;
+        beTrue(result);
+      });
+      it('single value check', function(){
+        result = ['a', 'b'].includes('b');
+        beTrue(result);
+
+        result = ['a', 'b'].includes(['a']);
+        beTrue(result);
+      });
+      it('multi value check', function(){
+        result = ['a', 'b', 'c'].includes(['a', 'b', 'c']);
+        beTrue(result);
+      });
+      it('false check', function(){
+        result = ['a'].includes('b');
+        beFalse(result);
+
+        result = [].includes('c');
+        beFalse(result);
+
+        result = [].includes(['a', 1]);
+        beFalse(result);
+      });
     });
 
-    it('array of arrays', function(){
-      var origin = [[1], [2, 2], [3, 3, [3, 3, 3]]];
-      var copy = origin.clone();
 
-      should(copy[0][0]).equal(1);
-      should(copy[1][1]).equal(2);
-      should(copy[2][2][2]).equal(3);
+    describe('.clone()', function(){
+      it('property exists', function(){
+        should([]).have.property('clone');
+        should([].clone).should.be.Function;
+      });
 
-      should(copy.length).equal(origin.length);
-      should(copy[0]).equal(origin[0]);
-      should(copy[1]).equal(origin[1]);
-      should(copy[2]).equal(origin[2]);
+      it('single item', function(){
+        var origin = [1];
+        var copy = origin.clone();
+
+        should(copy[0]).equal(1);
+        should(copy.length).equal(origin.length);
+        should(copy[0]).equal(origin[0]);
+      });
+
+      it('many items', function(){
+        var origin = [1, 2, 3];
+        var copy = origin.clone();
+
+        should(copy[0]).equal(1);
+        should(copy[1]).equal(2);
+        should(copy[2]).equal(3);
+        should(copy.length).equal(origin.length);
+        should(copy[0]).equal(origin[0]);
+        should(copy[1]).equal(origin[1]);
+        should(copy[2]).equal(origin[2]);
+      });
+
+      it('many objects', function(){
+        var origin = [{}, {}, {}];
+        var copy = origin.clone();
+
+        should(copy.length).equal(origin.length);
+        should(copy[0]).equal(origin[0]);
+        should(copy[1]).equal(origin[1]);
+        should(copy[2]).equal(origin[2]);
+      });
+
+      it('array of arrays', function(){
+        var origin = [[1], [2, 2], [3, 3, [3, 3, 3]]];
+        var copy = origin.clone();
+
+        should(copy[0][0]).equal(1);
+        should(copy[1][1]).equal(2);
+        should(copy[2][2][2]).equal(3);
+
+        should(copy.length).equal(origin.length);
+        should(copy[0]).equal(origin[0]);
+        should(copy[1]).equal(origin[1]);
+        should(copy[2]).equal(origin[2]);
+      });
     });
   });
 });
--- test/console.js
+++ test/console.js
@@ -11,74 +11,95 @@ function beFalse(result) {
   should(result).be.false();
 }
 
-describe('console', function(){
-  it('.pipe()', function(){
-    should(console).have.property('pipe');
-    should(console.pipe).be.a.Function;
+describe('console', function() {
+  describe('.pipe()', function() {
+    it('property exists', function() {
+      should(console).have.property('pipe');
+      should(console.pipe).be.a.Function;
+    });
 
-    var one = console.pipe(1);
-    var fourfive = console.pipe(4, 5);
+    it('first argument returns', function() {
+      var one = console.pipe(1);
+      var fourfive = console.pipe(4, 5);
 
-    should(one).be.a.Number;
-    should(one).equal(1);
+      should(one).be.a.Number;
+      should(one).equal(1);
 
-    should(fourfive).be.a.Number;
-    should(fourfive).equal(4);
+      should(fourfive).be.a.Number;
+      should(fourfive).equal(4);
+    });
   });
 
-  it('.log.pipe()', function(){
-    should(console.log).have.property('pipe');
-    should(console.log.pipe).be.a.Function;
+  describe('.log.pipe()', function() {
+    it('property exists', function() {
+      should(console.log).have.property('pipe');
+      should(console.log.pipe).be.a.Function;
+    });
 
-    var one = console.log.pipe(1);
-    var fourfive = console.log.pipe(4, 5);
+    it('first argument returns', function() {
+      var one = console.log.pipe(1);
+      var fourfive = console.log.pipe(4, 5);
 
-    should(one).be.a.Number;
-    should(one).equal(1);
+      should(one).be.a.Number;
+      should(one).equal(1);
 
-    should(fourfive).be.a.Number;
-    should(fourfive).equal(4);
+      should(fourfive).be.a.Number;
+      should(fourfive).equal(4);
+    });
   });
 
-  it('.error.pipe()', function(){
-    should(console.error).have.property('pipe');
-    should(console.error.pipe).be.a.Function;
+  describe('.info.pipe()', function() {
+    it('property exists', function() {
+      should(console.info).have.property('pipe');
+      should(console.info.pipe).be.a.Function;
+    });
 
-    var one = console.error.pipe(1);
-    var fourfive = console.error.pipe(4, 5);
+    it('first argument returns', function() {
+      var one = console.info.pipe(1);
+      var fourfive = console.info.pipe(4, 5);
 
-    should(one).be.a.Number;
-    should(one).equal(1);
+      should(one).be.a.Number;
+      should(one).equal(1);
 
-    should(fourfive).be.a.Number;
-    should(fourfive).equal(4);
+      should(fourfive).be.a.Number;
+      should(fourfive).equal(4);
+    });
   });
 
-  it('.warn.pipe()', function(){
-    should(console.warn).have.property('pipe');
-    should(console.warn.pipe).be.a.Function;
+  describe('.warn.pipe()', function() {
+    it('property exists', function() {
+      should(console.warn).have.property('pipe');
+      should(console.warn.pipe).be.a.Function;
+    });
 
-    var one = console.warn.pipe(1);
-    var fourfive = console.warn.pipe(4, 5);
+    it('first argument returns', function() {
+      var one = console.warn.pipe(1);
+      var fourfive = console.warn.pipe(4, 5);
 
-    should(one).be.a.Number;
-    should(one).equal(1);
+      should(one).be.a.Number;
+      should(one).equal(1);
 
-    should(fourfive).be.a.Number;
-    should(fourfive).equal(4);
+      should(fourfive).be.a.Number;
+      should(fourfive).equal(4);
+    });
   });
 
-  it('.info.pipe()', function(){
-    should(console.info).have.property('pipe');
-    should(console.info.pipe).be.a.Function;
+  describe('.error.pipe()', function() {
+    it('property exists', function() {
+      should(console.error).have.property('pipe');
+      should(console.error.pipe).be.a.Function;
+    });
 
-    var one = console.info.pipe(1);
-    var fourfive = console.info.pipe(4, 5);
+    it('first argument returns', function() {
+      var one = console.error.pipe(1);
+      var fourfive = console.error.pipe(4, 5);
 
-    should(one).be.a.Number;
-    should(one).equal(1);
+      should(one).be.a.Number;
+      should(one).equal(1);
 
-    should(fourfive).be.a.Number;
-    should(fourfive).equal(4);
+      should(fourfive).be.a.Number;
+      should(fourfive).equal(4);
+    });
   });
+
 });
